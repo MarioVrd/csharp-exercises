8f0794183ab463a51177a1bd1d8aef409c74328c
Mario <vrdoljak.mario98@gmail.com>
2020-03-13 14:00:09 +0100

    Initial setup for exercise 02

---
+++ Exercise02/Task1/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+	public abstract class Shape
+	{
+		protected double xPos;
+		protected double yPos;
+
+		public abstract double getArea();
+		public abstract double getPerimeter();
+		public abstract void printData();
+
+		public double getXPos()
+		{
+			return xPos;
+		}
+
+		public double getYPos()
+		{
+			return yPos;
+		}
+	}
+}
---
+++ Exercise02/Task1/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+	public class Square : Shape
+	{
+		private double oneSide;
+
+		public Square(double p)
+		{
+			oneSide = p;
+			xPos = DataModel.getNewXPos();
+			yPos = DataModel.getNewYPos();
+		}
+
+		public override double getArea()
+		{
+			return oneSide * oneSide;
+		}
+
+		public override double getPerimeter()
+		{
+			return 4 * oneSide;
+		}
+
+		public override void printData()
+		{
+			Console.WriteLine();
+			Console.WriteLine("My type: " + this.GetType());
+			Console.WriteLine("Side of square = " + oneSide);
+			Console.WriteLine("X position = " + xPos);
+			Console.WriteLine("Y position = " + yPos);
+		}
+	}
+}
