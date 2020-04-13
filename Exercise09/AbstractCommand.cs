c88912448a0368ec9403507857d620c74b124063
Mario <vrdoljak.mario98@gmail.com>
2020-04-13 14:44:32 +0200

    Initial setup for Exercise 09

---
+++ Exercise09/AbstractCommand.cs
@@ -0,0 +1,12 @@
+namespace Labs
+{
+	/// <summary>
+	/// Summary description for AbstractCommand.
+	/// </summary>
+	public abstract class AbstractCommand
+	{
+		public abstract void doit();
+		public abstract void undo();
+		public abstract void redo();
+	}
+}
